70e9ca33d0497e9f2da6d9ff71e43b23d13cda83
marshallein <maxboiz123hn@gmail.com>
2022-06-22 20:08:52 +0700

    feat: customize tab buttons in sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -8,23 +8,73 @@ const Sidebar = () => {
 
   return (
     <div className={wapper}>
-      <div className="flex flex-col">
-        <div className="basis-20 flex">
-          <div>
-            <Image src="/avatar.svg" height={50} width={50}></Image>
+      <div className="flex flex-col space-y-4 justify-center items-center">
+        <div className="basis-20 flex py-2 space-x-4">
+          <div className="">
+            <div className="border-2 rounded-full p-3 w-16 h-16">
+              <Image src="/avatar.svg" height={45} width={45}></Image>
+            </div>
           </div>
-          <div className='flex flex-col pl-4'>
+          <div className="flex flex-col justify-center">
             <div>
-                <p>an tran</p>
+              <p>an tran</p>
             </div>
             <div>
-                <p>aloalo@gmail.com</p>
+              <p>aloalo@gmail.com</p>
             </div>
           </div>
         </div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 4</div>
+        {/* Todo: fix search bar responsive (if needed) */}
+        <div className="">
+          <div className="pt-2 relative mx-auto text-gray-600">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
+            ></input>
+            <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
+              <Image src="/search.svg" height={18} width={18}></Image>
+            </button>
+          </div>
+        </div>
+        <div className="rounded-md flex justify-between items-center w-60 hover:bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 px-2 py-2">
+          <div className="flex gap-4">
+            <div>🤡</div>
+            <div>My Day</div>
+          </div>
+          <div className="rounded-full bg-neutral-500 flex justify-center items-center px-3 py-1 text-sm">
+            5
+          </div>
+        </div>
+        <div className="rounded-md flex justify-between items-center w-60 hover:bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 px-2 py-2">
+          <div className="flex gap-4">
+            <div>🤡</div>
+            <div>Important</div>
+          </div>
+          <div className="rounded-full bg-neutral-500 flex justify-center items-center px-3 py-1 text-sm">
+            5
+          </div>
+        </div>
+        <div className="rounded-md flex justify-between items-center w-60 hover:bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 px-2 py-2">
+          <div className="flex gap-4">
+            <div>🤡</div>
+            <div>Planned</div>
+          </div>
+          <div className="rounded-full bg-neutral-500 flex justify-center items-center px-3 py-1 text-sm">
+            5
+          </div>
+        </div>
+        <div className="rounded-md flex justify-between items-center w-60 hover:bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 px-2 py-2">
+          <div className="flex gap-4">
+            <div>🤡</div>
+            <div>Tasks</div>
+          </div>
+          <div className="rounded-full bg-neutral-500 flex justify-center items-center px-3 py-1 text-sm">
+            5
+          </div>
+        </div>
+        <hr className="border-neutral-600 w-60"></hr>
       </div>
     </div>
   )
